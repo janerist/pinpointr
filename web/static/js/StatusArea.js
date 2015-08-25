12d7256462a086a332edd98d0e2b1071aa30e20a
janerist <jan.erik.strom@gmail.com>
2015-08-26 00:27:40 +0200

    Remove unneeded function

--- web/static/js/StatusArea.js
+++ web/static/js/StatusArea.js
@@ -11,25 +11,6 @@ let StatusArea = React.createClass({
     });
   },
 
-  getGameStateMessage(gameState) {
-    switch (gameState) {
-      case "waiting_for_players":
-        return "Waiting for players...";
-      case "game_starting":
-        return "New game is starting...";
-      case "round_starting":
-        return "New round is starting...";
-      case "round_in_progress":
-        return "Round in progress";
-      case "round_ended":
-        return "Round finished";
-      case "game_ended":
-        return "Game finished";
-      default: 
-        return "";
-    }
-  },
-
   render() {
     return (
       <div className="well well-sm text-center">
