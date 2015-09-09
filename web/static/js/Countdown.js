9b871367ccc6872819db8a56b5659cb27b03312c
janerist <jan.erik.strom@gmail.com>
2015-09-09 09:58:18 +0200

    Add ability to colorize countdown

--- web/static/js/Countdown.js
+++ web/static/js/Countdown.js
@@ -1,13 +1,16 @@
 let Countdown = React.createClass({
 	render() {
-		var colorClass = "label-success";
+		var colorClass = "label-primary";
 
-		if (this.props.countdown < 7) {
-			colorClass = "label-warning";
-		} 
-		if (this.props.countdown < 4) {
-			colorClass = "label-danger";
+		if (this.props.colorize) {
+			if (this.props.countdown < 7) {
+				colorClass = "label-warning";
+			} 
+			if (this.props.countdown < 4) {
+				colorClass = "label-danger";
+			}
 		}
+
 		return (
 			<span className={"label " + colorClass}
 				  style={{fontSize: 16}}>
--- web/static/js/StatusArea.js
+++ web/static/js/StatusArea.js
@@ -24,11 +24,11 @@ let StatusArea = React.createClass({
 
   render() {
     return (
-      <div className="alert alert-warning">
+      <div className="well well-sm">
         <div className="container-fluid">
           <div className="row statusArea">
             <div className="col-md-2">
-              <Countdown countdown={this.state.countdown} />
+              <Countdown countdown={this.state.countdown} colorize={true} />
             </div>
             <div className="col-md-8 text-center">
               <div className="statusMessage">
@@ -36,7 +36,7 @@ let StatusArea = React.createClass({
               </div>
             </div>
             <div className="col-md-2 text-right">
-              <Countdown countdown={this.state.countdown} />
+              <Countdown countdown={this.state.countdown} colorize={true} />
             </div>
           </div>
         </div>
