c284e48c6ae663852b787e177fbec53543badd7c
janerist <jan.erik.strom@gmail.com>
2015-09-10 20:08:36 +0200

    Production config

---
+++ config/prod.secret.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :pinpointr, Pinpointr.Endpoint,
+  secret_key_base: System.get_env("PINPOINTR_SECRET") 
+
+# Configure your database
+config :pinpointr, Pinpointr.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "PINPOINTR_DB_URL"},
+  pool_size: 20
